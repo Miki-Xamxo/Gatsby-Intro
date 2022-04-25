6841a63e2494982d581c3f4f74837bad9cd6231d
Mikail <miki.xamxoev@mail.ru>
2022-04-25 14:32:22 +0300

    fix

--- src/components/seo.js
+++ src/components/seo.js
@@ -59,7 +59,7 @@ function Seo({ description, lang, meta, title }) {
         },
         {
           name: `twitter:card`,
-          content: `summary`,
+          content: `summary_large_image`,
         },
         {
           name: `twitter:creator`,
