793debf6ee6795d4a48e36fa0adff2b2a69de8a7
Mikail <miki.xamxoev@mail.ru>
2022-10-09 20:05:46 +0300

    event load

--- src/pages/index.js
+++ src/pages/index.js
@@ -7,21 +7,29 @@ import { useLocation } from '@reach/router';
 import Layout from "../components/layout"
 import Seo from "../components/seo"
 // import Iframe from "../components/iframe";
-import loadable from '@loadable/component';
+// import loadable from '@loadable/component';
 
-const Iframe = loadable(() => import('../components/iframe'));
+// const Iframe = loadable(() => import('../components/iframe'));
 
 
 const IndexPage = () => {
   const location = useLocation();
+  const [loading, setLoading] = React.useState(false)
   console.log(location) 
+
+  React.useEffect(() => {
+    window.addEventListener("load", setLoading(true));
+  }, [])
 
   return <Layout>
     <Seo title="Home" description='Welcome to your new Gatsby site. Kick off your next' />
     <h1>Hi people One</h1>
     <p>Welcome to your new Gatsby site.</p>
     <p>Now go build something great.</p>
-    <Iframe fallback={<div>Loading...</div>} />
+    {/* <Iframe fallback={<div>Loading...</div>} /> */}
+    {!loading && <h1>Loading...</h1>}
+    <iframe width="560" height="315" src={ loading ? "https://www.youtube.com/embed/zi9GUHrQj2U?controls=0&amp;autoplay=1&amp;mute=1&amp;loop=1&amp;playlist=zi9GUHrQj2U" : ''} title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
+
 
     <StaticImage
       src="../images/gatsby-astronaut.png"
