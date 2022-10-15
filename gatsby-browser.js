9a27ce0b47675540e5ed279d6228d63a1198b182
Mikail <miki.xamxoev@mail.ru>
2022-10-15 15:26:09 +0300

    lazy load video

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -5,3 +5,23 @@
  */
 
 // You can delete this file if you're not using it
+// export const onInitialClientRender = () => {
+//   // wait to init GTM until after React has hydrated in this lifecycle
+//     console.log("ReactDOM.render has executed")
+//     const dataLayer = window.dataLayer || []
+//     dataLayer.push({ "base.start": new Date().getTime(), event: "base.js" })
+// }
+
+export const onInitialClientRender = () => { 
+    // let iframe = document.querySelectorAll('iframe[src]');
+    let iframe = document.querySelector('.iframe');
+    iframe.src = iframe.dataset.src
+    iframe.removeAttribute('data-src')
+
+    // iframe.forEach((v) => {
+    //     console.log(v)
+
+    //     v.setAttribute('src', v.data);
+    //     v.removeAttribute('data');
+    // });
+}
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,36 +1,23 @@
 import * as React from "react"
 import { Link } from "gatsby"
 import { StaticImage } from "gatsby-plugin-image"
-import { useLocation } from '@reach/router';
 
 
 import Layout from "../components/layout"
 import Seo from "../components/seo"
-// import Iframe from "../components/iframe";
-// import loadable from '@loadable/component';
-
-// const Iframe = loadable(() => import('../components/iframe'));
 
 
 const IndexPage = () => {
-  const location = useLocation();
-  const [loading, setLoading] = React.useState(false)
-  console.log(location) 
-
-  React.useEffect(() => {
-    window.addEventListener("load", setLoading(true));
-  }, [])
-
   return <Layout>
     <Seo title="Home" description='Welcome to your new Gatsby site. Kick off your next' />
     <h1>Hi people One</h1>
     <p>Welcome to your new Gatsby site.</p>
     <p>Now go build something great.</p>
-    {/* <Iframe fallback={<div>Loading...</div>} /> */}
-    {!loading && <h1>Loading...</h1>}
-    <iframe width="560" height="315" src={ loading ? "https://www.youtube.com/embed/zi9GUHrQj2U?controls=0&amp;autoplay=1&amp;mute=1&amp;loop=1&amp;playlist=zi9GUHrQj2U" : ''} title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
+    <iframe className="iframe"  width="560" height="315" data-src='https://www.youtube.com/embed/zi9GUHrQj2U?controls=0&amp;autoplay=1&amp;mute=1&amp;loop=1&amp;playlist=zi9GUHrQj2U' title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
+    {/* <video src={vimeo.srcset[0].link} controls autoPlay loop /> */}
 
-
+    {/* <iframe src="https://player.vimeo.com/video/759133513?h=6fd9098f14&amp;badge=0&amp;autopause=0&amp;player_id=0&amp;app_id=58479" width="3840" height="2160" frameborder="0" allow="autoplay; fullscreen; picture-in-picture" allowfullscreen title="signax.mp4"></iframe> */}
+    
     <StaticImage
       src="../images/gatsby-astronaut.png"
       width={300}
